d40a359f15b34e6718d47d5c401c4b4c76ffc57b
abhinav1912 <abhinavmathur1912@gmail.com>
2025-07-27 10:42:09 +0530

    refactor: moved configs from after to plugins

--- nvim/.config/nvim/after/plugin/fzf-citc.lua
+++
@@ -1,7 +0,0 @@
-local fzfc = require("fzf-citc")
-
-if not fzfc then return end
-
-vim.keymap.set("n", "<leader>cmf", fzfc.modified, { desc = "fuzzy CitC modified files picker" })
-vim.keymap.set("n", "<leader>cmd", fzfc.modified_dirs, { desc = "fuzzy CitC modified directory switcher" })
-vim.keymap.set("n", "<leader>cws", fzfc.workspaces, { desc = "fuzzy CitC workspace switcher" })
--- nvim/.config/nvim/lua/plugins/google/fzf-citc.lua
+++ nvim/.config/nvim/lua/plugins/google/fzf-citc.lua
@@ -1,5 +1,12 @@
 return {
-  -- 'vintharas/fzf-codesearch.nvim',
-  url = 'sso://user/aktau/fzf-citc.nvim',
-  dependencies = { "ibhagwan/fzf-lua" }
+    -- 'vintharas/fzf-codesearch.nvim',
+    url = 'sso://user/aktau/fzf-citc.nvim',
+    dependencies = { "ibhagwan/fzf-lua" },
+    config = function()
+        local fzfc = require("fzf-citc")
+
+        vim.keymap.set("n", "<leader>cmf", fzfc.modified, { desc = "[C]itC [m]odified [f]iles picker" })
+        vim.keymap.set("n", "<leader>cmd", fzfc.modified_dirs, { desc = "[C]itC [m]odified [d]irectory switcher" })
+        vim.keymap.set("n", "<leader>cws", fzfc.workspaces, { desc = "[C]itC [w]orkspace [s]witcher" })
+    end,
 }
