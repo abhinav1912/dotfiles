65b309265000d19329699d291f3d02569af3076a
Abhinav Mathur <abhistract@google.com>
2025-08-25 19:42:07 +0530

    updated line number color

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,9 @@
+function LineNumberColors()
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=false })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=false })
+end
+
 vim.cmd("source ~/.vimrc")
 require("abhinavmathur")
+LineNumberColors()
