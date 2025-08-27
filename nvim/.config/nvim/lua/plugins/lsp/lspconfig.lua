3c7e716e9e3be825737c4e12196720f457f17f4f
abhinav1912 <abhinavmathur1912@gmail.com>
2025-08-27 23:30:21 +0530

    updated lspconfig

--- nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -2,6 +2,9 @@ return {
     {
         "neovim/nvim-lspconfig",
         event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+        },
         config = function()
             local lspconfig = require('lspconfig')
             lspconfig.sourcekit.setup {
@@ -20,6 +23,25 @@ return {
                     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap = true, silent = true})
                     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {noremap = true, silent = true})
                 end,
+            })
+
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+            local keymap = vim.keymap -- for conciseness
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+
+            vim.diagnostic.config({
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = " ",
+                        [vim.diagnostic.severity.WARN] = " ",
+                        [vim.diagnostic.severity.HINT] = "󰠠 ",
+                        [vim.diagnostic.severity.INFO] = " ",
+                    },
+                },
+            })
+            vim.lsp.config("*", {
+                capabilities = capabilities,
             })
         end,
     }
