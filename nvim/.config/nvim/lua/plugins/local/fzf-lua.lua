4f6c7c85d0889fffcebb442ac904f80d5b24609c
abhinav1912 <abhinavmathur1912@gmail.com>
2025-08-30 23:51:01 +0530

    added fzf keymaps

--- nvim/.config/nvim/lua/plugins/local/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/local/fzf-lua.lua
@@ -1,5 +1,17 @@
 return {
     "ibhagwan/fzf-lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {}
+    opts = {},
+    config = function ()
+        local fzf = require("fzf-lua")
+
+        vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "[f]ind [f]ile" })
+        vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "[f]uzzy [g]rep" })
+        vim.keymap.set("n", "<leader>fk", fzf.keymaps, { desc = "[f]ind [k]eymaps" })
+        vim.keymap.set("n", "<leader>fw", fzf.grep_cword, { desc = "[f]ind [w]ord" })
+        vim.keymap.set("n", "<leader>fW", fzf.grep_cWORD, { desc = "[f]ind [W]ord" })
+        vim.keymap.set("n", "<leader>fd", fzf.diagnostics_document, { desc = "[f]ind [d]iagnostics" })
+        vim.keymap.set("n", "<leader>fo", fzf.oldfiles, { desc = "[f]ind [o]ldfiles" })
+        vim.keymap.set("n", "<leader>/", fzf.lgrep_curbuf, { desc = "[/] grep current buffer" })
+    end
 }
